889930ee6ad7cc92a6e319349a10bde73f0ae323
Michael Leadley <leadley75@gmail.com>
2017-08-22 11:34:44 +0100

    Further refactor to minimise code

--- models/rps_game.rb
+++ models/rps_game.rb
@@ -7,17 +7,9 @@ class RPSGame
   HANDS = WINNERS.keys
 
   def self.play(hand1, hand2)
-    return -1 unless valid_hands?(hand1, hand2)
+    return -1 unless HANDS.include?(hand1) && HANDS.include?(hand2)
     return 0 if hand1 == hand2
-    return 1 if is_winner?(hand1, hand2)
+    return 1 if WINNERS[hand1] == hand2
     return 2
-  end
-
-  def self.is_winner?(hand1, hand2)
-    WINNERS[hand1] == hand2
-  end
-
-  def self.valid_hands?(hand1, hand2)
-    HANDS.include?(hand1) && HANDS.include?(hand2)
   end
 end
--- specs/rps_game_spec.rb
+++ specs/rps_game_spec.rb
@@ -3,22 +3,6 @@ require "minitest/rg"
 require_relative "../models/rps_game"
 
 class RPSGameSpec < MiniTest::Test
-  def test_rock_scissors_is_valid
-    assert_equal(true, RPSGame.valid_hands?(:rock, :scissors))
-  end
-
-  def test_rock_biscuit_is_invalid
-    assert_equal(false, RPSGame.valid_hands?(:rock, :biscuit))
-  end
-
-  def test_rock_scissors_is_winner
-    assert_equal(true, RPSGame.is_winner?(:rock, :scissors))
-  end
-
-  def test_scissors_rock_is_not_winner
-    assert_equal(false, RPSGame.is_winner?(:scissors, :rock))
-  end
-
   def test_play_rock_scissors_is_1
     assert_equal(1, RPSGame.play(:rock, :scissors))
   end
