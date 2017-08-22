91f1cafb7bfa74537c721a22f1029b2376556476
Michael Leadley <leadley75@gmail.com>
2017-08-22 13:28:27 +0100

    Further minimising

--- models/rps_game.rb
+++ models/rps_game.rb
@@ -4,10 +4,9 @@ class RPSGame
     :scissors => :paper,
     :paper => :rock
   }
-  HANDS = WINNERS.keys
 
   def self.play(hand1, hand2)
-    return -1 unless HANDS.include?(hand1) && HANDS.include?(hand2)
+    return -1 unless WINNERS.has_key?(hand1) && WINNERS.has_key?(hand2)
     return 0 if hand1 == hand2
     return 1 if WINNERS[hand1] == hand2
     return 2
